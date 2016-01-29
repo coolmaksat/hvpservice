f8c4d2724a358dc487d6a0e0b081dec556ea4315
Maxat Kulmanov <coolmaksat@gmail.com>
2016-01-29 22:49:21 +0300

    added models

--- hvpservice/apps/hvp/models.py
+++ hvpservice/apps/hvp/models.py
@@ -2,4 +2,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Query(models.Model):
+
+    vcf_file = models.FileField(upload_to='vcfs/%Y/%m/%d/')
+    disease = models.CharField(max_length=64)
+
+
+class Result(models.Model):
+    pass
