4336f19f938c9b2b8f92c9224ac65b108bae71a0
Maxat Kulmanov <coolmaksat@gmail.com>
2016-01-29 19:26:43 +0300

    initial commit

---
+++ manage.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import os
+import sys
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_CONFIGURATION", "Development")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hvpservice.settings")
+
+    from configurations.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
